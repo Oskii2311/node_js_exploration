3006ec68c36d4faca173d5d14cfcb5db4681eb4c
oskii2311 <os.janczak@gmail.com>
2018-06-23 14:29:56 +0200

    m

--- front_end/src/components/List/List.jsx
+++ front_end/src/components/List/List.jsx
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 import Record from '../../containers/Record/Record';
 import './List.css';
 
-const List = ({header, Loading, Notes}) => (
+const List = ({ header, Loading, Notes }) => (
   <div>
     <div className="header">
       <h1>{header}</h1>
     </div>
     <div className="container_todo">
-      {Loading
-        ? 'Loading...'
-        : Notes.map(note => (<Record key={note._id} note={note} header={header}/>))
-}
+      {
+        Loading ?
+        'Loading...' :
+        Notes.map(note => (
+          <Record key={note._id} note={note} header={header} />))
+      }
     </div>
   </div>
 );
@@ -20,13 +22,13 @@ const List = ({header, Loading, Notes}) => (
 List.defaultProps = {
   header: '',
   Loading: false,
-  Notes: []
+  Notes: [],
 };
 
 List.propTypes = {
   header: PropTypes.string,
   Loading: PropTypes.bool,
-  Notes: PropTypes.arrayOf(PropTypes.object)
+  Notes: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default List;

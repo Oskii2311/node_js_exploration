3e93c8ae8322f19e6bf4872742b887a827a06757
oskii2311 <os.janczak@gmail.com>
2018-06-23 17:58:07 +0200

    code wrap

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -6,6 +6,6 @@ import App from './containers/App/App';
 import './index.css';
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>, document.getElementById('root'));
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root'),
+);
